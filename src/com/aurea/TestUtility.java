57cd60d4816222caf9c08bad03aea8dcf147d3c3
Cristhian Recalde <cristhian.recalde@aurea.com>
2018-02-14 10:05:09 -0300

    nested switch

--- src/com/aurea/TestUtility.java
+++ src/com/aurea/TestUtility.java
@@ -2,12 +2,38 @@ package com.aurea;
 
 // test for auto
 // checking
+
 import com.sun.org.apache.bcel.internal.classfile.Utility;
 
 public class TestUtility extends Utility {
 
-    public void test() {
+    public void test(int b) {
         System.out.println(signatureToString("this is a test string", true));
+
+        int m = (int) Math.pow(b, 2) / 2 + 7;
+        switch (b) {
+            case 0:
+            case 1:
+                System.out.println("Invalid");
+                return;
+
+            case 2:
+                switch (m) {
+                    case 9:
+                        System.out.println("nince");
+                        return;
+                    case 10:
+                        System.out.println("TEN!");
+                        break;
+                    default:
+                        System.out.println("really weird number");
+                        break;
+                }
+                break;
+            case 3:
+                System.out.println("Nothing to do");
+                break;
+        }
     }
 
 }
